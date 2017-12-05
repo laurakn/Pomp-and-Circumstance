3985b432a151ecab4ed1c22ebcf77b245198103c
Laurakn <laura.niss@gmail.com>
2017-12-04 22:12:47 -0500

    initial commit

---
+++ R/circumstance.R
@@ -0,0 +1,16 @@
+#' circumstance requires pomp
+#'
+#' This function allows one to have pomp and circumstance
+#'
+#' @return None
+#'
+#' @keywords pomp, circumstance
+#'
+#' @examples
+#' cirsumstance()
+#'
+#' @export
+
+circumstance <- function(){
+  require("pomp")
+}
